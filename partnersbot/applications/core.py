661f62a1af63a295fb5f43d4c980a1d0eb587ee2
Jori van Ee <jorivanee@hotmail.com>
2018-03-23 10:11:31 +0100

    Pushed code, probably won't work lol

--- partnersbot/applications/core.py
+++ partnersbot/applications/core.py
@@ -1,12 +1,20 @@
-class Application:
-    pass
+from collections import namedtuple
+
+Application = namedtuple("Application", 'id approved author fields timestamp deny_reason')
 
 class ApplicationManager:
     def __init__(self, bot):
         self.bot = bot
-        self.metadata = bot.db.metadata
+        if bot.config.database.enabled:
+            self.metadata = bot.db.metadata
+
+    def get_channel(self, id):
+        channel = self.bot.get_channel(id=id)
+        return channel
 
     async def _get_request_id(self):
+        if not hasattr(self, "metadata"):
+            return None
         casecount_doc = await self.metadata.find_one({'_id': "request_count"})
         if not casecount_doc:
             await self.metadata.insert_one({"_id": "request_count", "value": 0})
@@ -14,3 +22,9 @@ class ApplicationManager:
         casecount = casecount_doc['value'] + 1
         await self.metadata.replace_one({'_id': "request_count"},{'value': casecount})
         return casecount
+
+    async def create_application(self, author, fields):
+        pass
+
+    async def get_application(self, _id):
+        pass
